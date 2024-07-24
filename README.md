f0f1a86fc20597c9c03dba34c945fbed1b538008
Cuboid <fanjas112358@gmail.com>
2024-07-24 14:29:50 -0400

    shims!
    stupid ahhh lazy person scripts I wrote
    will probably detonate if not linux/KDE6
    pacmanclean will obv not work if not arch/arch-based

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dotfiles
-my .dotfiles omg crazy
+my .dotfiles omg crazy \
+note to self: ./shims is pasted from /shim, *not* symlinked, so it needs to be updated manually because I don't wanna deal with permissions
---
+++ shims/add.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# only works for linux, and files in home directory of current user
+# dotfiles repo bust also be in /home/$USER/dotfiles
+
+import sys
+import getpass
+import shutil
+import os
+import re
+import typing
+
+tgt_file: str = os.path.abspath(sys.argv[1])
+strip_user: typing.Pattern = re.compile(r".*" + getpass.getuser() + r"/(.*)")
+stripped_path: str = strip_user.match(tgt_file).group(1)
+
+dotfiles_dir = f"/home/{getpass.getuser()}/dotfiles/dotfiles/" # change for different dotfile repo location
+
+try:
+    os.mkdir(dotfiles_dir)
+
+except FileExistsError:
+    print("DEBUG: dotfiles/dotfiles/ exists")
+
+os.makedirs(os.path.dirname(dotfiles_dir + stripped_path), exist_ok=True)
+
+shutil.move(tgt_file, dotfiles_dir + stripped_path)
+
+os.symlink(dotfiles_dir + stripped_path, tgt_file)
+
+print(f"DEBUG: Moved/symlinked to/from {dotfiles_dir + stripped_path}")
+print("Done!")
