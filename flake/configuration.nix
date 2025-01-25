ba15d862498c7ee2e71228309a49980802f09cad
Cuboid <fanjas112358@gmail.com>
2025-01-25 10:24:53 -0500

    nix flake
    we'll see how this goes

---
+++ flake/configuration.nix
@@ -0,0 +1,159 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+    imports =
+        [ # Include the results of the hardware scan.
+            ./hardware-configuration.nix
+            inputs.home-manager.nixosModules.default
+        ];
+
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    # sudo nix-shell -p refind efibootmgr sbsigntool and then refind-install for rEFInd.
+
+    networking.hostName = "dregsdesk10"; # Define your hostname.
+    # networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
+
+    # Enable networking
+    networking.networkmanager.enable = true;
+
+    # Set your time zone.
+    time.timeZone = "America/Toronto";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_CA.UTF-8";
+
+    # Enable the X11 windowing system.
+    # You can disable this if you're only using the Wayland session.
+    services.xserver.enable = true;
+
+    # Enable the KDE Plasma Desktop Environment.
+    services.displayManager.sddm.enable = true;
+    services.desktopManager.plasma6.enable = true;
+
+    # Enable magic sysrq key
+    boot.kernel.sysctl = {
+        "kernel.sysrq" = 438;
+    };
+
+    # Configure keymap in X11
+    services.xserver.xkb = {
+        layout = "us";
+        variant = "";
+    };
+
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
+
+    # Enable sound with pipewire.
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        #jack.enable = true;
+
+        # use the example session manager (no others are packaged yet so this is enabled by default,
+        # no need to redefine it in your config for now)
+        #media-session.enable = true;
+    };
+
+    # Enable touchpad support (enabled default in most desktopManager).
+    # services.xserver.libinput.enable = true;
+
+    # Define a user account. Don't forget to set a password with ‘passwd’.
+    users.users.jason = {
+        isNormalUser = true;
+        description = "Jason Fan";
+        extraGroups = [ "networkmanager" "wheel" ];
+        packages = with pkgs; [
+            kdePackages.kate
+        #    thunderbird
+        ];
+    };
+
+    # Install firefox.
+    programs.firefox.enable = true;
+
+    # Allow unfree packages
+    nixpkgs.config.allowUnfree = true;
+
+    # sublime text 4 and some other packages need it
+    nixpkgs.config.permittedInsecurePackages = [
+        "openssl-1.1.1w"
+    ];
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+        github-desktop
+        sublime4
+        gparted
+        firefox-devedition-bin
+        vesktop
+        speedcrunch
+        anki-bin
+        flameshot
+
+        eza
+        starship
+        bat
+        fastfetch
+        xclip
+        htop
+        neovim
+    ];
+
+    fonts.packages = with pkgs; [
+        cascadia-code
+    ];
+
+    hardware.graphics = {
+        enable = true;
+    };
+    services.xserver.videoDrivers = [ "nvidia" ];
+    hardware.nvidia = {
+        modesetting.enable = true;
+        powerManagement.enable = true;
+        open = false;
+        nvidiaSettings = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #     enable = true;
+    #     enableSSHSupport = true;
+    # };
+
+    # List services that you want to enable:
+
+    # Enable the OpenSSH daemon.
+    # services.openssh.enable = true;
+
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # networking.firewall.enable = false;
+
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "24.11"; # Did you read the comment?
+
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+}
---
+++ flake/flake.nix
@@ -0,0 +1,25 @@
+{
+    description = "Default flake";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = { self, nixpkgs, ... }@inputs:
+    let
+        system = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+        nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+            specialArgs = {inherit inputs;};
+            modules = [
+                ./configuration.nix
+            ];
+        };
+    };
+}
