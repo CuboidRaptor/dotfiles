ac0bbd0472f571daa0853e2cad43d196e369989d
Cuboid <fanjas112358@gmail.com>
2025-01-29 16:51:06 -0500

    moved keyd to keyd.nix
    was getting a bit long

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,6 +9,7 @@
     ./hardware-configuration.nix
     ./nvidia.nix
     ./pkgs.nix
+    ./keyd.nix
   ];
 
   # flakes!
@@ -138,55 +139,7 @@
     extraGroups = [
       "networkmanager"
       "wheel"
-      "keyd"
     ];
-  };
-
-  # keyd config! also, autoset numlock
-  services.displayManager.sddm.autoNumlock = true;
-  services.keyd = {
-    enable = true;
-    keyboards = {
-      default = {
-        ids = [ "*" ];
-        settings = {
-          main = {
-            capslock = "esc";
-            rightalt = "overload(alt, f5)";
-            pageup = "home";
-            pagedown = "end";
-            home = "pageup";
-            end = "pagedown";
-            esc = "layer(nump)";
-          };
-          shift = {
-            capslock = "capslock";
-          };
-          nump = {
-            comma = "kp0";
-            dot = "kpdot";
-            slash = "^";
-            k = "kp1";
-            l = "kp2";
-            semicolon = "kp3";
-            i = "kp4";
-            o = "kp5";
-            p = "kp6";
-            "8" = "kp7";
-            "9" = "kp8";
-            "0" = "kp9";
-            rightshift = "kpplus";
-            apostrophe = "kpminus";
-            leftbrace = "kpasterisk";
-            minus = "kpslash";
-            m = "kpleftparen";
-            j = "kprightparen";
-            u = "macro(a n s)";
-            "7" = "numlock";
-          };
-        };
-      };
-    };
   };
 
   # Some programs need SUID wrappers, can be configured further or are
---
+++ nixos/keyd.nix
@@ -0,0 +1,56 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, inputs, ... }:
+
+{
+  services.displayManager.sddm.autoNumlock = true; # enable numlock by default
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        ids = [ "*" ];
+        settings = {
+          main = {
+            capslock = "esc";
+            rightalt = "overload(alt, f5)"; # rAlt is alt but only when held
+            pageup = "home";
+            pagedown = "end";
+            home = "pageup";
+            end = "pagedown";
+            esc = "layer(nump)"; # this enable numpad layer
+          };
+          shift = {
+            capslock = "capslock"; # shift caps enables caps
+          };
+          nump = { # numpad!
+            comma = "kp0";
+            dot = "kpdot";
+            slash = "^";
+            k = "kp1";
+            l = "kp2";
+            semicolon = "kp3";
+            i = "kp4";
+            o = "kp5";
+            p = "kp6";
+            "8" = "kp7";
+            "9" = "kp8";
+            "0" = "kp9";
+
+            rightshift = "kpplus";
+            apostrophe = "kpminus";
+            leftbrace = "kpasterisk";
+            minus = "kpslash";
+
+            m = "kpleftparen";
+            j = "kprightparen";
+            u = "macro(a n s)";
+            "7" = "numlock";
+          };
+        };
+      };
+    };
+  };
+  users.users.jason.extraGroups = [ "keyd" ];
+}
