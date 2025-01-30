b73fedfafcbaf52d0012222b033482327c6b4eb1
Cuboid <fanjas112358@gmail.com>
2025-01-29 21:04:29 -0500

    literally v block
    barely used and replaceable

--- dotfiles/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/.config/nvim/lua/config/keymaps.lua
@@ -2,39 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- funni visual selection go brrrr
-local function comment() -- block commenting code -- language-specific
-    local s_start = vim.fn.getpos("v")[2]
-    local s_end = vim.fn.getpos(".")[2]
-    local js = math.abs(s_end - s_start)
-    local jstring
-    
-    if (js == 0)
-    then
-        jstring = ""
-    else
-        jstring = tostring(js) .. "j"
-    end
-    
-    return "<Esc>:'<<CR>0<C-v>" .. jstring .. "I##<Esc>"
-end
-
-local function uncomment() -- uncomment
-    local s_start = vim.fn.getpos("v")[2]
-    local s_end = vim.fn.getpos(".")[2]
-    local js = math.abs(s_end - s_start)
-    local jstring
-    
-    if (js == 0)
-    then
-        jstring = ""
-    else
-        jstring = tostring(js) .. "j"
-    end
-    
-    return "<Esc>:'<<CR>0<C-v>l" .. jstring .. ":s/##/<CR>"
-end
-
 leader_1 = "," -- leader key go brrrr
 
 vim.keymap.set("n", "x", "\"_x", { remap=false, desc="Delete" }) -- the x key no longer goes to any register
@@ -42,9 +9,6 @@ vim.keymap.set("v", "x", "\"_x", { remap=false, desc="Delete" })
 
 vim.keymap.set("n", "<C-a>", "gg_vG$", { remap=true, desc="Select All" })
 vim.keymap.set("i", "<C-a>", "<Esc>gg_vG$", { remap=true, desc="Select All" })
-
-vim.keymap.set("v", leader_1 .. "c", function() return comment() end, { remap=true, desc="Block Comment", expr=true })
-vim.keymap.set("v", leader_1 .. "u", function() return uncomment() end, { remap=true, desc="Block Uncomment", expr=true })
 
 vim.keymap.set("n", "<Home>", "^", { remap=true, desc="Go home, after indents" })
 vim.keymap.set("i", "<Home>", "<Esc>^i", { remap=true, desc="Go home, after indents" })
