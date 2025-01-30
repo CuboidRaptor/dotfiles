0870c558df793c9b407312803305a706e166033b
Cuboid <fanjas112358@gmail.com>
2025-01-29 21:01:12 -0500

    formatting change

--- dotfiles/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/.config/nvim/lua/config/keymaps.lua
@@ -37,22 +37,22 @@ end
 
 leader_1 = "," -- leader key go brrrr
 
-vim.keymap.set("n", "x", "\"_x", {remap=false, desc="Delete"}) -- the x key no longer goes to any register
-vim.keymap.set("v", "x", "\"_x", {remap=false, desc="Delete"})
+vim.keymap.set("n", "x", "\"_x", { remap=false, desc="Delete" }) -- the x key no longer goes to any register
+vim.keymap.set("v", "x", "\"_x", { remap=false, desc="Delete" })
 
-vim.keymap.set("n", "<C-a>", "gg_vG$", {remap=true, desc="Select All"})
-vim.keymap.set("i", "<C-a>", "<Esc>gg_vG$", {remap=true, desc="Select All"})
+vim.keymap.set("n", "<C-a>", "gg_vG$", { remap=true, desc="Select All" })
+vim.keymap.set("i", "<C-a>", "<Esc>gg_vG$", { remap=true, desc="Select All" })
 
-vim.keymap.set("v", leader_1 .. "c", function() return comment() end, {remap=true, desc="Block Comment", expr=true})
-vim.keymap.set("v", leader_1 .. "u", function() return uncomment() end, {remap=true, desc="Block Uncomment", expr=true})
+vim.keymap.set("v", leader_1 .. "c", function() return comment() end, { remap=true, desc="Block Comment", expr=true })
+vim.keymap.set("v", leader_1 .. "u", function() return uncomment() end, { remap=true, desc="Block Uncomment", expr=true })
 
-vim.keymap.set("n", "<Home>", "^", {remap=true, desc="Go home, after indents"})
-vim.keymap.set("i", "<Home>", "<Esc>^i", {remap=true, desc="Go home, after indents"})
-vim.keymap.set("v", "<Home>", "^", {remap=true, desc="Go home, after indents"})
+vim.keymap.set("n", "<Home>", "^", { remap=true, desc="Go home, after indents" })
+vim.keymap.set("i", "<Home>", "<Esc>^i", { remap=true, desc="Go home, after indents" })
+vim.keymap.set("v", "<Home>", "^", { remap=true, desc="Go home, after indents" })
 
 -- code runner!
-vim.keymap.set("n", "<F5>", ":w<CR>:RunCode<CR>i", {remap=true, desc="Run Code"})
-vim.keymap.set("i", "<F5>", "<Esc>:w<CR>:RunCode<CR>i", {remap=true, desc="Run Code"})
+vim.keymap.set("n", "<F5>", ":w<CR>:RunCode<CR>i", { remap=true, desc="Run Code" })
+vim.keymap.set("i", "<F5>", "<Esc>:w<CR>:RunCode<CR>i", { remap=true, desc="Run Code" })
 
 -- allow Esc in terminal mode
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {remap=true, desc="Escape in Terminal Mode"})
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { remap=true, desc="Escape in Terminal Mode" })
