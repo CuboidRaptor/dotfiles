cebce956a2ed1f543ccc4aa68f456f1155fa3b7a
Cuboid <fanjas112358@gmail.com>
2025-02-01 23:13:26 -0500

    configure blink
    super tab, max autocomplete 1 items, change docs popup max height

---
+++ dotfiles/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "saghen/blink.cmp",
+        opts = {
+            completion = {
+                list = {
+                    max_items = 1
+                },
+                documentation = {
+                    window = {
+                        max_height = 8
+                    }
+                }
+            },
+            keymap = {
+                preset = "super-tab"
+            },
+        }
+    }
+}
