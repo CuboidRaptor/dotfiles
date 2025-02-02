e1877555219340b250a1a8b25115d8b71aeda90c
Cuboid <fanjas112358@gmail.com>
2025-02-01 22:21:41 -0500

    delete unnecessary keybinds
    ggVG and :%y+ work and these keymaps are buggy as shit anyways

--- dotfiles/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/.config/nvim/lua/config/keymaps.lua
@@ -4,9 +4,6 @@
 
 leader_1 = "," -- leader key go brrrr
 
-vim.keymap.set("n", "<C-a>", "gg0vG$", { remap=true, desc="Select All" })
-vim.keymap.set("i", "<C-a>", "<Esc>gg0vG$", { remap=true, desc="Select All" })
-
 vim.keymap.set("n", "<Home>", "^", { remap=true, desc="Go home, after indents" })
 vim.keymap.set("i", "<Home>", "<Esc>^i", { remap=true, desc="Go home, after indents" })
 vim.keymap.set("v", "<Home>", "^", { remap=true, desc="Go home, after indents" })
@@ -20,6 +17,3 @@ vim.keymap.set("i", "<F5>", "<Esc>:w<CR>:RunCode<CR>i", { remap=true, desc="Run 
 vim.keymap.set("t", "<F5>", "<Esc><C-w><C-k>:w<CR>:RunCode<CR>i", { remap=true, desc="Run Code" })
     -- allow reruns directly from terminal
 
--- yank all
-vim.keymap.set("n", leader_1 .. "a", "mqgg0vG$\"+y`q:delm q<CR>")
-vim.keymap.set("i", leader_1 .. "a", "<Esc>mqgg0vG$\"+y`q:delm q<CR>a")
