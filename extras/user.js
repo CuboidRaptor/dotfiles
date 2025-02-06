32e792f6d1e052516a953f501e511ee685c852e0
Cuboid <fanjas112358@gmail.com>
2025-02-05 21:53:39 -0500

    reenabling gfx canvas
    firefox crashes out on tons of images and literally thrashes my system
    on a completely unrelated note systemd-oomd is trash

--- extras/user.js
+++ extras/user.js
@@ -283,7 +283,7 @@ user_pref("browser.urlbar.trimHttps", false);
 user_pref("browser.urlbal.trimURLs", false);
 
 // PREF_CUSTOM: this breaks stuff when it's true (desmos, google docs, google maps, etc.)
-user_pref("gfx.canvas.accelerated", false);
+///user_pref("gfx.canvas.accelerated", false);
 
 // PREF_CUSTOM: sideloading extensions go brrrrr (only for firefox dev)
 user_pref("xpinstall.signatures.required", false);
