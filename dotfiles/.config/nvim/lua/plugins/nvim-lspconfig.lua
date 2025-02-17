80c450b59cb83a926825766e3444ec69fd1db3e8
Cuboid <fanjas112358@gmail.com>
2025-02-16 23:12:32 -0500

    changed to lazyvim langs
    better support, more stuff
    some stuff still remains in LSP only

--- dotfiles/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dotfiles/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,15 +3,9 @@ return {
         "neovim/nvim-lspconfig",
         opts = {
             servers = {
-                pyright = {},
-                ruff = {},
                 jsonls = {},
-                clangd = {},
-                jdtls = {},
                 html = {},
                 cssls = {},
-                ts_ls = {},
-                eslint = {},
                 lua_ls = {},
                 bashls = {}
             }
