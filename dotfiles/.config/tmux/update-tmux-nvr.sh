8ef5e51bb293c38529d5421cdd8ea9e3d4a48326
Cuboid <fanjas112358@gmail.com>
2025-02-17 19:03:44 -0500

    remove tmux-nvr

--- dotfiles/.config/tmux/update-tmux-nvr.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# requires git-subtree
-cd "$(git rev-parse --show-toplevel)" || exit
-git subtree pull --prefix dotfiles/.config/tmux/tmux-nvr https://github.com/carlocab/tmux-nvr main --squash
