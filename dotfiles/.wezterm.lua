b1ff1ecd4d87a579fd1ff6be4f543d6d906054e4
Cuboid <fanjas112358@gmail.com>
2025-02-24 18:52:05 -0500

    remove redundant stuff
    mouse and keyboard bindings that I don't and will never use
    + increase padding because why not

--- dotfiles/.wezterm.lua
+++ dotfiles/.wezterm.lua
@@ -22,36 +22,10 @@ config.font = wezterm.font_with_fallback {
 config.font_size = 12
 
 config.window_padding = {
-    left = "10px",
-    right = "10px",
-    top = "10px",
-    bottom = "10px",
-}
-
--- copy/paste with mouse go brrrrrr
-local act = wezterm.action
-config.mouse_bindings = {
-    {
-        event = { Down = { streak = 1, button = "Right" } },
-        mods = "NONE",
-        action = wezterm.action_callback(function(window, pane)
-        local has_selection = window:get_selection_text_for_pane(pane) ~= ""
-        if has_selection then
-            window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
-            window:perform_action(act.ClearSelection, pane)
-        else
-            window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
-        end
-        end),
-    },
-}
-
-config.keys = {
-    { key = "q", mods = "CTRL|SHIFT", action = wezterm.action.QuitApplication },
-    { key = "{", mods = "SHIFT|ALT", action = act.MoveTabRelative(-1) },
-    { key = "}", mods = "SHIFT|ALT", action = act.MoveTabRelative(1) },
-    { key = "Tab", mods = "CTRL", action = act.DisableDefaultAssignment }, -- unbind a buncha keys so I can use them in neovim
-    { key = "Tab", mods = "CTRL|SHIFT", action = act.DisableDefaultAssignment }
+    left = "15px",
+    right = "15px",
+    top = "15px",
+    bottom = "15px",
 }
 
 -- Open new wezterms in existing instances if possible
