ab0611c4a9db5bbae12c693f572de2b6e98408e2
Cuboid <fanjas112358@gmail.com>
2025-02-23 21:26:49 -0500

    change to <cmd>
    in case you change your cmd key idfk

--- dotfiles/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/.config/nvim/lua/config/keymaps.lua
@@ -14,19 +14,19 @@ vim.keymap.set("n", " ", "a <Esc>", { remap = true, desc = "Insert Space" })
 -- allow Esc in terminal mode
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { remap = true, desc = "Escape in Terminal Mode" })
 
-vim.keymap.set("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { remap = true, desc = "Previous Buffer" })
-vim.keymap.set("n", "<Tab>", ":BufferLineCycleNext<CR>", { remap = true, desc = "Next Buffer" })
+vim.keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", { remap = true, desc = "Previous Buffer" })
+vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", { remap = true, desc = "Next Buffer" })
 
 -- code runner!
-vim.keymap.set("n", "<F5>", ":w<CR>:RunCode<CR>i", { remap = true, desc = "Run Code" })
-vim.keymap.set("i", "<F5>", "<Esc>:w<CR>:RunCode<CR>i", { remap = true, desc = "Run Code" })
-vim.keymap.set("t", "<F5>", "<Esc><C-w><C-k>:w<CR>:RunCode<CR>i", { remap = true, desc = "Run Code" })
+vim.keymap.set("n", "<F5>", "<Cmd>w<CR><Cmd>RunCode<CR>i", { remap = true, desc = "Run Code" })
+vim.keymap.set("i", "<F5>", "<Esc><Cmd>w<CR><Cmd>RunCode<CR>i", { remap = true, desc = "Run Code" })
+vim.keymap.set("t", "<F5>", "<Esc><C-w><C-k><Cmd>w<CR><Cmd>RunCode<CR>i", { remap = true, desc = "Run Code" })
 -- allow reruns directly from terminal
 
 vim.keymap.set("i", "<Leader>h", "#!/usr/bin/env ", { remap = true, desc = "Env Shebang" })
 vim.keymap.set("n", "<Leader>h", "a#!/usr/bin/env ", { remap = true, desc = "Env Shebang and Insert" })
 
-vim.keymap.set("n", "<Leader>w", ":SudaWrite", { remap = true, desc = "Save with Sudo" })
+vim.keymap.set("n", "<Leader>w", "<Cmd>SudaWrite", { remap = true, desc = "Save with Sudo" })
 
 -- Enter in normal mode now just inserts a newline on the next line without leaving normal
 vim.keymap.set("n", "<CR>", "o<Esc>", { remap = true, desc = "Insert Newline" })
@@ -36,7 +36,7 @@ vim.keymap.set("n", "<Leader>z", "@z", { remap = true, desc = "Send Z Macro" })
 vim.keymap.set("n", "<Leader>b", "0d^i<BS>", { remap = true, desc = "Append current line to last one and insert" })
 
 -- Telescope bindings
-vim.keymap.set("n", "tb", ":Telescope buffers<CR>", { remap = true, desc = "Telescope Buffers" })
-vim.keymap.set("n", "tg", ":Telescope git_files<CR>", { remap = true, desc = "Telescope Git Repo Files" })
-vim.keymap.set("n", "tf", ":Telescope find_files<CR>", { remap = true, desc = "Telescope Current Directory" })
-vim.keymap.set("n", "tt", ":Telescope treesitter<CR>", { remap = true, desc = "Telescope Treesitter" })
+vim.keymap.set("n", "tb", "<Cmd>Telescope buffers<CR>", { remap = true, desc = "Telescope Buffers" })
+vim.keymap.set("n", "tg", "<Cmd>Telescope git_files<CR>", { remap = true, desc = "Telescope Git Repo Files" })
+vim.keymap.set("n", "tf", "<Cmd>Telescope find_files<CR>", { remap = true, desc = "Telescope Current Directory" })
+vim.keymap.set("n", "tt", "<Cmd>Telescope treesitter<CR>", { remap = true, desc = "Telescope Treesitter" })
