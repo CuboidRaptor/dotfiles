2cf3befd1072c9d6bad6dc70595724578f0a67bc
Cuboid <fanjas112358@gmail.com>
2025-02-23 21:41:26 -0500

    I broke it
    idk it just runs if I don't define empty opts
    anyways

--- dotfiles/.config/nvim/lua/plugins/auto-session.lua
+++ dotfiles/.config/nvim/lua/plugins/auto-session.lua
@@ -2,5 +2,12 @@ return {
     {
         "rmagatti/auto-session",
         lazy = false,
+
+        ---enables autocomplete for opts
+        ---@module "auto-session"
+        ---@type AutoSession.Config
+        opts = {
+            -- log_level = 'debug',
+        }
     }
 }
