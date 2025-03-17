1d1d629eeee3eea3556a5ab25dfd3c76afeb4c53
Cuboid <fanjas112358@gmail.com>
2025-03-16 21:38:27 -0400

    discord/vesktop theme

--- link.py
+++ link.py
@@ -33,6 +33,7 @@ PATHS: list[str] = [
     ".config/sublime-text/Packages/User/",
     ".config/VSCodium/User/",
     ".config/gtk-3.0/gtk.css",
+    ".config/vesktop/themes/"
     ".config/flameshot/",
     ".config/wireplumber/wireplumber.conf.d/51-disable-suspension.conf",
     ".config/mimeapps.list",
