fb7a9e25f859a7158010e59e42849d6467109528
Cuboid <fanjas112358@gmail.com>
2025-03-29 16:56:04 -0400

    change name of winepin
    just make it follow the same pattern as standard pkgs but with the
    winepinned prefix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -4,12 +4,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
-    winepin.url = "github:NixOS/nixpkgs/0e82ab234249d8eee3e8c91437802b32c74bb3fd";
+    winepinnednixpkgs.url = "github:NixOS/nixpkgs/0e82ab234249d8eee3e8c91437802b32c74bb3fd";
   };
 
-  outputs = { self, nixpkgs, nix-flatpak, winepin, ... }@inputs: let
+  outputs = { self, nixpkgs, nix-flatpak, winepinnednixpkgs, ... }@inputs: let
     system = "x86_64-linux";
-    winepinnedpkgs = import winepin {
+    winepinnedpkgs = import winepinnednixpkgs {
       inherit system;
       config.allowUnfree = true;
     };
