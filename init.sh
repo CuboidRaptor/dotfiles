c1c93bfb0cc80bf70427523a0f3a9671dfc8d585
Cuboid <fanjas112358@gmail.com>
2025-03-30 14:58:36 -0400

    add -eo pipefail
    extra reliability

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # load hardware config, switch nix, create distrobox, and link dotfiles.
 
+set -eo pipefail
 sudo cp /etc/nixos/hardware-configuration.nix "$HOME/dotfiles/nixos/hardware-configuration.nix"
 sudo nixos-rebuild switch --flake "path://$HOME/dotfiles/nixos#dregsdesk15"
 distrobox create --name dregsbox --init --image registry.opensuse.org/opensuse/distrobox:latest --additional-packages "python3 python3-pipx"
