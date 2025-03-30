c136716915d98e669f8f7ba4d6d613194385e2cd
Cuboid <fanjas112358@gmail.com>
2025-03-30 13:55:24 -0400

    ehhh may as well do distrobox as well

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@
 sudo cp /etc/nixos/hardware-configuration.nix "$HOME/dotfiles/nixos/hardware-configuration.nix"
 sudo nixos-rebuild switch --flake "path://$HOME/dotfiles/nixos#dregsdesk15"
 distrobox create --name dregsbox --init --image registry.opensuse.org/opensuse/distrobox:latest --additional-packages "python3 python3-pipx"
+distrobox enter dregsbox -- pipx install bpython
 python3 "$HOME/dotfiles/link.py"
-echo "Note: distrobox apps still need to be manually installed and dbox-setup'd."
 
