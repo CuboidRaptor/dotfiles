c363194ff211f3134b500ae173878952b956e1b4
Cuboid <fanjas112358@gmail.com>
2025-04-05 10:24:40 -0400

    variuos improvements to dbox-setup.sh
    use different functions, avoid splitting/globbing, add whitespace, make
    grep pattern more compact, etc.

--- dbox-setup.sh
+++ dbox-setup.sh
@@ -5,24 +5,27 @@
 set -eo pipefail
 
 # list of pipx things you want to export
-pyapps=($(ls "$HOME/.local/share/pipx/venvs/"))
+mapfile -t pyapps <<< "$(ls "$HOME/.local/share/pipx/venvs/")"
 dboxpath="$HOME/dotfiles/dboxexports"
 
 # clean out $dboxpath
 rm -rf "$dboxpath"
 mkdir "$dboxpath"
+
 for dir in "${pyapps[@]}"
 do
     cd "$HOME/.local/share/pipx/venvs/$dir/bin/"
     # loop over every file in current pipx folder, filter out python/activation scripts
-    files=($(ls | grep -v '^activate' | grep -v '^Activate' | grep -v '^python[23]\?'))
+    mapfile -t files <<< "$(ls | grep -iv "^activate\|^python[23]\?")"
     for bin in "${files[@]}"
     do
         distrobox-export --bin "$(pwd)/$bin" --export-path "$dboxpath"
-        mv "$HOME/.local/bin/$bin" "~/.local/bin/${bin}_distrobox_bak" || :
+        mv "$HOME/.local/bin/$bin" "$HOME/.local/bin/${bin}_distrobox_bak" || :
     done
 done
+
 # export python to boxpython3 and boxpython
 distrobox-export --bin "/usr/bin/python3" --export-path "$dboxpath"
 mv "$dboxpath/python3" "$dboxpath/boxpython3"
 ln -s "$dboxpath/boxpython3" "$dboxpath/boxpython"
+echo "Renamed python* -> boxpython*"
