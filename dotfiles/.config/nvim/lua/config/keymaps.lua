63606850d4ab6e544c8e8bd8d9397dda7ea3c129
Cuboid <fanjas112358@gmail.com>
2025-04-10 18:39:25 -0400

    change space binding and formatting
    the space binding now makes sense (it now inserts a space at the
    beginning of the cursor like how most block cursors behave)
    also format some of the code

--- dotfiles/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/.config/nvim/lua/config/keymaps.lua
@@ -13,7 +13,7 @@ map("i", "<Home>", "<Esc>^i", { remap = true, desc = "Go home, after indents" })
 map("v", "<Home>", "^", { remap = true, desc = "Go home, after indents" })
 
 -- space key inserts space in normal mode
-map("n", " ", "a <Esc>", { remap = true, desc = "Insert Space" })
+map("n", " ", "i <Esc>l", { remap = true, desc = "Insert Space" })
 
 -- allow Esc in terminal mode
 map("t", "<Esc>", "<C-\\><C-n>", { remap = true, desc = "Escape in Terminal Mode" })
@@ -31,7 +31,7 @@ map("n", leader .. "q", "@q", { remap = true, desc = "Send Q Macro" })
 map("n", leader .. "r", "*<S-n>cgn", { remap = true, desc = "Replace occurences of current word" })
 
 -- Telescope bindings
-map("n", leader .. "b", function() telescope.buffers({sort_mru=true, path_display={"filename_first", "truncate"}}) end, { remap = true, desc = "Telescope Buffers" })
-map("n", leader .. "g", function() telescope.git_files({path_display={"filename_first", "truncate"}}) end, { remap = true, desc = "Telescope Git Repo Files" })
-map("n", leader .. "f", function() telescope.find_files({path_display={"filename_first", "truncate"}}) end, { remap = true, desc = "Telescope Current Directory" })
+map("n", leader .. "b", function() telescope.buffers({ sort_mru = true, path_display = { "filename_first", "truncate" } }) end, { remap = true, desc = "Telescope Buffers" })
+map("n", leader .. "g", function() telescope.git_files({ path_display = { "filename_first", "truncate" } }) end, { remap = true, desc = "Telescope Git Repo Files" })
+map("n", leader .. "f", function() telescope.find_files({ path_display = { "filename_first", "truncate" } }) end, { remap = true, desc = "Telescope Current Directory" })
 map("n", leader .. "t", telescope.treesitter, { remap = true, desc = "Telescope Treesitter" })
