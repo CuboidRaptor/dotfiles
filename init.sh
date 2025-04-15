9c6fbf24d8e56792c139b163e412d7550559f190
Cuboid <fanjas112358@gmail.com>
2025-04-15 18:54:13 -0400

    add tkinter to boxpython

--- init.sh
+++ init.sh
@@ -4,7 +4,7 @@
 set -eo pipefail
 sudo cp /etc/nixos/hardware-configuration.nix "$HOME/dotfiles/nixos/hardware-configuration.nix"
 sudo nixos-rebuild switch --flake "path://$HOME/dotfiles/nixos#dregsdesk15"
-distrobox create --name dregsbox --init --image registry.opensuse.org/opensuse/distrobox:latest --additional-packages "python3 python3-pipx"
+distrobox create --name dregsbox --init --image registry.opensuse.org/opensuse/distrobox:latest --additional-packages "python3 python3-pipx python3-tk"
 distrobox enter dregsbox -- pipx install bpython
 distrobox enter dregsbox -- "$HOME/dotfiles/dbox-setup.sh"
 python3 "$HOME/dotfiles/link.py"
