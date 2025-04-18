f5e981ff1c0a77738722429876426ed5391904c1
Cuboid <fanjas112358@gmail.com>
2025-04-18 11:49:38 -0400

    fix keymap
    `` works better in edge cases and is less buggy

--- dotfiles/.config/nvim/lua/config/keymaps.lua
+++ dotfiles/.config/nvim/lua/config/keymaps.lua
@@ -28,8 +28,8 @@ map("n", "<CR>", "o<Esc>", { remap = true, desc = "Insert Newline" })
 
 map("n", leader .. "q", "@q", { remap = true, desc = "Send Q Macro" })
 
-map("n", leader .. "r", "*<S-n>cgn", { remap = true, desc = "Replace occurences of current word" })
-map("v", leader .. "r", "*<S-n>cgn", { remap = true, desc = "Replace occurences of current word" })
+map("n", leader .. "r", "*``cgn", { remap = true, desc = "Replace occurences of current word" })
+map("v", leader .. "r", "*``cgn", { remap = true, desc = "Replace occurences of current word" })
 
 -- Telescope bindings
 map("n", leader .. "b",
