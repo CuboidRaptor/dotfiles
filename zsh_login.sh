27c93aa0e8db14cc02e4142343eb285c5abf8893
Cuboid <fanjas112358@gmail.com>
2025-04-26 20:17:11 -0400

    format and set $SHELL
    it sets $SHELL to the default to make it make even more sense
    and some formatting because I like it

--- zsh_login.sh
+++ zsh_login.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+set -o pipefail
+
 # run zsh in a blank login shell and set $PATH to that $PATH
 # this basically sets $PATH to how it would be in a new login shell
-# patch for tmuxp's weird behaviour
-newpath="$(env -i HOME="$HOME" zsh --login -c 'echo "$PATH"')"
-export PATH="$newpath" && zsh "$@"
+# (it's a patch for tmuxp's weird behaviour)
+# and also set $SHELL to login default cuz that's get screwed up too
+defaultpath="$(env -i HOME="$HOME" zsh --login -c 'echo "$PATH"')" \
+    && export PATH="$defaultpath" \
+    && defaultshell="$(getent passwd "$USER" | awk -F: '{print $NF}')" \
+    && export SHELL="$defaultshell" \
+    && exec -a "zsh" zsh "$@" # set $0
