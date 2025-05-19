f54b11b007824d06a2cdd30a4c68df035e942367
Cuboid <fanjas112358@gmail.com>
2025-05-19 18:55:50 -0400

    nvm
    average scuffed nixpkgs moment

--- nixos/pkgs.nix
+++ nixos/pkgs.nix
@@ -141,7 +141,11 @@
       obs-studio
       wineWowPackages.staging
       winetricks
-      vivaldi
+      (vivaldi.overrideAttrs (oldAttrs: {
+        dontWrapQtApps = false;
+        dontPatchELF = true;
+        nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.kdePackages.wrapQtAppsHook ];
+      }))
       vivaldi-ffmpeg-codecs
       remmina
       pavucontrol
