94d7c27f2120cf2fa2bec8d3e1220a4fabc6c8e8
Cuboid <fanjas112358@gmail.com>
2025-07-30 21:41:30 -0400

    remove nixos folder
    don't need it, was only here before for porting purposes as this branch
    was forked from nixos

--- nixos/nixfmt_script.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# recursively nixfmt all nix files in current directory except for hardware-configuration
-# it just break idk why
-fd -u ".*\.nix" | grep -v "hardware-configuration.nix" | xargs nixfmt
