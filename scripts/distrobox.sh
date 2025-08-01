7b5fb9d7719d0ae5653c0c33fe83b2da46f15b53
Cuboid <fanjas112358@gmail.com>
2025-07-31 23:35:51 -0400

    fix reaper in distrobox
    yippeex

--- scripts/distrobox.sh
+++ scripts/distrobox.sh
@@ -20,7 +20,8 @@ sudo mkdir -p /run/dbus
 sudo ln -s /run/host/run/dbus/system_bus_socket /run/dbus || true
 
 # Install libs/deps and apps
-paru -S mesa vlc pipewire-jack xapp bibata-cursor-theme vesktop reaper
+paru -S mesa vlc pipewire-jack xapp libxml2-legacy xcb-util-xrm xcb-util-cursor bibata-cursor-theme \
+    vesktop reaper
 
 # Export apps
 distrobox-export --app vesktop
